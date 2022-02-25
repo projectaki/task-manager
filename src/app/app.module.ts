f0959e85543b477ed179360600e497e84dfa4c08
Akos <akosegypro@gmail.com>
2022-02-25 21:53:14 +0100

    :heavy_plus_sign: Add primeng + ui libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ProjectsModule } from './projects/projects.module';
 import { HomeModule } from './home/home.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, ProjectsModule, HomeModule],
+  imports: [BrowserModule, FlexLayoutModule, BrowserAnimationsModule, AppRoutingModule, ProjectsModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
