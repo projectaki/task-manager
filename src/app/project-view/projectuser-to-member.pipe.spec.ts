81eb7dcfaf9e31f300000379a64a9bb29f4ee0fb
Akos <akosegypro@gmail.com>
2022-03-11 20:36:54 +0100

    :heavy_plus_sign: Add test for project to member pipe

--- src/app/project-view/projectuser-to-member.pipe.spec.ts
+++ src/app/project-view/projectuser-to-member.pipe.spec.ts
@@ -1,8 +1,29 @@
+import { ProjectRole } from '../core/enums/project-role.enum';
+import { ProjectUser } from '../core/models/project-user.interface';
 import { ProjectuserToMemberPipe } from './projectuser-to-member.pipe';
+import { Member } from './../members/member';
 
 describe('ProjectuserToMemberPipe', () => {
   it('create an instance', () => {
     const pipe = new ProjectuserToMemberPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('create an instance', () => {
+    const pipe = new ProjectuserToMemberPipe();
+    const projectUsers: ProjectUser[] = [
+      {
+        id: '1',
+        accepted: false,
+        company: 'test',
+        email: 'test@test.com',
+        name: 'test',
+        role: ProjectRole.CLIENT,
+      },
+    ];
+    const res = pipe.transform(projectUsers, ProjectRole.OWNER);
+
+    expect(pipe).toBeTruthy();
+    expect(res.length).toBe(1);
   });
 });
